446ecb6215a6e1beb6850481d787141661f9a5e8
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-16 21:18:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
- word.scan(/^[aeiou]\w+/)? true : false
+ word.match(/^[aeiou]\w+/)? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.match()
 end
 
 def words_five_letters_long(text)
